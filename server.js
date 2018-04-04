6254a78faa6737e1aa0ffb196e65520e470ab0c6
parker lindley <fugazi8@gmail.com>
2018-04-04 17:54:20 -0600

    Add users endpoints

--- server.js
+++ server.js
@@ -9,10 +9,29 @@ const database = require('knex')(configuration);
 app.set('port', process.env.PORT || 3000);
 
 app.locals.title = 'eSpeak db';
-
 app.use(bodyParser.json());
 
+app.get('/api/v1/users', (request, response) => {
+  database('users').select()
+    .then( users => {
+      response.status(200).json(users);
+    })
+    .catch( error => {
+      response.status(500).json({ error })
+    })
+})
 
+app.get('/api/v1/users/:id', (request, response) => {
+  const { id } = request.params;
+
+  database('users').where('id', id).select()
+    .then( user => {
+      response.status(200).json(user)
+    })
+    .catch( error => {
+      response.status(500).json({ error })
+    })
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is listening at ${app.get('port')}`);
