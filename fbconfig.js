0259e2a762da7f10acf0ef27e699d58fe03280a7
Amanda Tjan <amanda.tjan@gmail.com>
2018-04-05 16:12:08 -0600

    Start setting up Firebase

---
+++ fbconfig.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+import keys from './keys.js';
+
+const config = {
+    apiKey: keys.firebase, 
+    authDomain: "espeak-53f28.firebaseapp.com",
+    databaseURL: "https://espeak-53f28.firebaseio.com",
+    projectId: "espeak-53f28",
+    storageBucket: "espeak-53f28.appspot.com",
+    messagingSenderId: "535165125077"
+  };
+export const firebaseApp = firebase.initializeApp(config);
