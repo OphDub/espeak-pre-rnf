c5f0736fdcf722e36d7c96298c7f45aeb06a6795
parker lindley <fugazi8@gmail.com>
2018-04-04 18:07:42 -0600

    Add GET for stack, words, users

--- server.js
+++ server.js
@@ -33,9 +33,40 @@ app.get('/api/v1/users/:id', (request, response) => {
     })
 })
 
+app.get('/api/v1/words', ( request, response ) => {
+  database('words').select()
+  .then( words => {
+    response.status(200).json(words);
+  })
+  .catch( error => {
+    response.status(500).json({ error });
+  })
+})
+
+app.get('/api/v1/stack', ( request, response ) => {
+  database('stack').select()
+  .then( stack => {
+    response.status(200).json(stack);
+  })
+  .catch( error => {
+    response.status(500).json({ error });
+  })
+})
+
+app.get('/api/v1/stack/:id', (request, response) => {
+  const { id } = request.params;
+
+  database('stack').where('id', id).select()
+  .then( stack => {
+    response.status(200).json(stack);
+  })
+  .catch( error => {
+    response.status(500).json({error})
+  })
+})
+
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is listening at ${app.get('port')}`);
 });
-
 
 module.exports = app;
