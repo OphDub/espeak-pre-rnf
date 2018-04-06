1980eb3f9e214d6d23f3d61358aba54c486e4420
Amanda Tjan <soytjan@gmail.com>
2018-04-06 14:43:26 -0600

    -m "Create mock data files for decks and words"

---
+++ mockData/words.js
@@ -0,0 +1,154 @@
+const words = [
+  {
+    english: 'hi',
+    spanish: 'hola',
+    hint: 'oh-la',
+    deck_id: 1
+  },
+  {
+    english: 'bye',
+    spanish: 'adios',
+    hint: 'ah-dee-ohs',
+    deck_id: 1
+  },
+  {
+    english: 'good morning',
+    spanish: 'buenos dias',
+    hint: 'wenos dee-ahz',
+    deck_id: 1
+  },
+  {
+    english: 'see you later',
+    spanish: 'hasta luego',
+    hint: 'ah-stah loo-eh-go',
+    deck_id: 1
+  },
+  {
+    english: 'good afternoon',
+    spanish: 'buenas tardes',
+    hint: 'wenos tahr-days',
+    deck_id: 1
+  },
+  {
+    english: 'good night',
+    spanish: 'buenas noches',
+    hint: 'wenos no-chayz',
+    deck_id: 1
+  },
+  {
+    english: 'how are you?',
+    spanish: 'como estas?',
+    hint: 'coh-mo eh-stas',
+    deck_id: 2
+  },
+  {
+    english: 'I am fine.',
+    spanish: 'Estoy bien.',
+    hint: 'coh-mo eh-stas',
+    deck_id: 2
+  },
+  {
+    english: 'What is your name?',
+    spanish: 'Como se llama?',
+    hint: 'coh-mo eh-stas',
+    deck_id: 2
+  },
+  {
+    english: 'Me llamo Bob',
+    spanish: 'My name is Bob.',
+    hint: 'coh-mo eh-stas',
+    deck_id: 2
+  },
+  {
+    english: 'apple',
+    spanish: 'manzana',
+    hint: 'coh-mo eh-stas',
+    deck_id: 3
+  },
+  {
+    english: 'banana',
+    spanish: 'platano',
+    hint: 'coh-mo eh-stas',
+    deck_id: 3
+  },
+  {
+    english: 'pepper',
+    spanish: 'pimiento',
+    hint: 'coh-mo eh-stas',
+    deck_id: 3
+  },
+  {
+    english: 'orange',
+    spanish: 'naranja',
+    hint: 'coh-mo eh-stas',
+    deck_id: 3
+  },
+  {
+    english: 'cucumber',
+    spanish: 'pepino',
+    hint: 'coh-mo eh-stas',
+    deck_id: 3
+  },
+  {
+    english: 'breakfast',
+    spanish: 'desayuno',
+    hint: 'coh-mo eh-stas',
+    deck_id: 4
+  },
+  {
+    english: 'lunch',
+    spanish: 'almuerzo',
+    hint: 'coh-mo eh-stas',
+    deck_id: 4
+  },
+  {
+    english: 'dinner',
+    spanish: 'cena',
+    hint: 'coh-mo eh-stas',
+    deck_id: 4
+  },
+  {
+    english: 'plate',
+    spanish: 'plato',
+    hint: 'coh-mo eh-stas',
+    deck_id: 4
+  },
+  {
+    english: 'food',
+    spanish: 'comida',
+    hint: 'coh-mo eh-stas',
+    deck_id: 4
+  },
+  {
+    english: 'dance',
+    spanish: 'bailar',
+    hint: 'coh-mo eh-stas',
+    deck_id: 5
+  },
+  {
+    english: 'run',
+    spanish: 'correr',
+    hint: 'coh-mo eh-stas',
+    deck_id: 5
+  },
+  {
+    english: 'walk',
+    spanish: 'caminar',
+    hint: 'coh-mo eh-stas',
+    deck_id: 5
+  },
+  {
+    english: 'jump',
+    spanish: 'saltar',
+    hint: 'coh-mo eh-stas',
+    deck_id: 5
+  },
+  {
+    english: 'eat',
+    spanish: 'comer',
+    hint: 'coh-mo eh-stas',
+    deck_id: 5
+  }
+] 
+
+module.exports = words;
