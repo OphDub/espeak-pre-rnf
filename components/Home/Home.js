2b72933d3e837856f71914998c1fce72f4c9036d
parker lindley <fugazi8@gmail.com>
2018-04-08 13:59:34 -0600

    Add logout button style settttings page

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  TouchableOpacity
 } from 'react-native';
 
 type Props = {};
@@ -14,10 +15,25 @@ export default class Home extends Component <Props> {
     this.state = {}
   }
 
+  logout = () => {
+    console.log('logout')
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>HOME</Text>
+        <Text style={styles.title}>SETTINGS</Text>
+        <View style={styles.statsDisplay}>
+          <Text style={styles.listItems}>Username: </Text>
+          <Text style={styles.listItems}>Current Points: </Text>
+          <Text style={styles.listItems}>Current Level:</Text>
+        </View>
+        <TouchableOpacity
+          onPress={this.logout}
+          style={styles.logoutBtn}
+        >
+          <Text style={styles.buttonText}>Logout</Text>
+        </TouchableOpacity>
       </View>
     )
   }
@@ -26,14 +42,44 @@ export default class Home extends Component <Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    backgroundColor: '#fff',
+  },
+  title: {
+    fontSize: 30,
+    textAlign: 'center',
+    color: 'purple',
+    marginTop: 60,
+  },
+  statsDisplay: {
+    display: 'flex',
+    alignSelf: 'center',
+    width: 280,
+    height: 180,
+    paddingTop: 20,
+    borderColor: '#1e3888',
+    borderWidth: 2,
+    borderRadius: 5, 
+  },
+  listItems: {
+    fontSize: 25,
+    margin: 5,
+    color: '#1e3888'
+  },
+  logoutBtn: {
+    display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'purple',
+    alignSelf: 'center',
+    margin: 10,
+    width: 150,
+    height: 30,
+    borderColor: '#1e3888',
+    borderWidth: 2,
+    borderRadius: 5,
   },
-  welcome: {
-    fontSize: 60,
-    textAlign: 'center',
-    color: 'white',
-    margin: 60,
-  },
+  buttonText: {
+    color: '#1e3888'
+  }
 });
