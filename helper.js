61923a8665c3ac27b2e350f29b564e39f8acf127
Ophus Wong <ophus.wong@gmail.com>
2018-04-09 17:51:22 -0600

    Remove unused methods and add verbAndParse func

--- helper.js
+++ helper.js
@@ -1,32 +1,12 @@
-import auth from './fbconfig';
-
-export const createUserInFb = async (email, password) => {
-  try {
-    const user = await auth.createUserWithEmailAndPassword(email, password);
-
-    console.log('User created!', user);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const signInUserFb = async (email, password) => {
-  try {
-    const user = await auth.signInWithEmailAndPassword(email, password);
-
-    console.log('Sign in successful', user);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const verbAndParse = (verb, url, data) => {
-  const initialFetch = fetch(url, {
+export const verbAndParse = async (verb, url, data) => {
+  const initialFetch = await fetch(url, {
     body: JSON.stringify(data),
     cache: 'no-cache',
     headers: {
       'content-type': 'application/json'
     },
-    method: `${verb.toUpperCase()}`
+    method: `${verb}`
   });
+
+  return await initialFetch.json();
 };
